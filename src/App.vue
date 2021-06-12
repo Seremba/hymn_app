e6b5271026d5b5231457f2427bbf9c5947aca317
Seremba <pserembae@gmail.com>
2021-06-12 21:25:14 +0300

    created the mark up of the app with ionic framework

--- src/App.vue
+++ src/App.vue
@@ -4,9 +4,10 @@
   </ion-app>
 </template>
 
-<script lang="ts">
+<script>
 import { IonApp, IonRouterOutlet } from '@ionic/vue';
 import { defineComponent } from 'vue';
+
 
 export default defineComponent({
   name: 'App',
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,68 +1,39 @@
 <template>
   <ion-page>
-    <ion-header :translucent="true">
+    <!-- <ion-header :translucent="true">
       <ion-toolbar>
         <ion-title>Blank</ion-title>
       </ion-toolbar>
-    </ion-header>
+    </ion-header> -->
     
     <ion-content :fullscreen="true">
-      <ion-header collapse="condense">
-        <ion-toolbar>
-          <ion-title size="large">Blank</ion-title>
-        </ion-toolbar>
-      </ion-header>
-    
-      <div id="container">
-        <strong>Ready to create an app?</strong>
-        <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
-      </div>
+      <ion-searchbar 
+      animated placeholder="search song" 
+      show-cancel-button="focus">
+      </ion-searchbar>
+     <app-buttons></app-buttons>
+      <ion-button expand="block" fill="outline">Open Song</ion-button>   
     </ion-content>
   </ion-page>
 </template>
 
-<script lang="ts">
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
+<script>
+import { IonContent, IonPage,  IonSearchbar, IonButton } from '@ionic/vue';
+import AppButtons from '../component/AppButtons.vue'
 import { defineComponent } from 'vue';
 
 export default defineComponent({
   name: 'Home',
   components: {
     IonContent,
-    IonHeader,
     IonPage,
-    IonTitle,
-    IonToolbar
+    IonSearchbar,
+    AppButtons,
+    IonButton
   }
 });
 </script>
 
-<style scoped>
-#container {
-  text-align: center;
-  
-  position: absolute;
-  left: 0;
-  right: 0;
-  top: 50%;
-  transform: translateY(-50%);
-}
+<style >
 
-#container strong {
-  font-size: 20px;
-  line-height: 26px;
-}
-
-#container p {
-  font-size: 16px;
-  line-height: 22px;
-  
-  color: #8c8c8c;
-  
-  margin: 0;
-}
-
-#container a {
-  text-decoration: none;
-}
 </style>
